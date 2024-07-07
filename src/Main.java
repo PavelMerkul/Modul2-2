9d53edd9cdff6e63a7e235e5226365405544515e
Pavel MSK <sonev@bk.ru>
2024-07-07 15:08:57 +0300

    commit lesson 3

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int initialBalance = 100; // начальная сумма на счету
+        int depositAmount = 1600; // сумма пополнения
+
+        int bonus; // переменная для хранения бонусных рублей
+
+        bonus = depositAmount / 100; // рассчитываем бонусные рубли
+
+        int finalBalance = initialBalance + depositAmount + bonus; // рассчитываем итоговый счет
+
+        System.out.println("Итоговый счет: " + finalBalance + " рублей");
+        System.out.println("Бонусные рубли: " + bonus + " рублей");
+    }
+
+}
